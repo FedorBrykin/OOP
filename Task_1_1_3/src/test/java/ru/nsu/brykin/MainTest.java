e007193b00da1acab7ed4044ebf597b794339bfb
RMS14579 <f.brykin@g.nsu.ru>
2024-09-22 19:05:07 +0700

    final2

--- Task_1_1_3/src/test/java/ru/nsu/brykin/MainTest.java
+++ Task_1_1_3/src/test/java/ru/nsu/brykin/MainTest.java
@@ -8,18 +8,18 @@ class MainTest {
     @Test
     public void parceTest() {
         Parser parser = new Parser();
-        Expression e = parser.parse("(3+(2*x))");
+        Expression e = parser.parse("(((2+(2*x))/2)-10)");
         final String standardOutput = e.toString();
-        assertTrue(standardOutput.contains("(3+(2*x))"));
+        assertTrue(standardOutput.contains("(((2+(2*x))/2)-10)"));
     }
 
     @Test
     public void parceTest2() {
         Parser parser = new Parser();
-        Expression e = parser.parse("(3+(2*x))");
+        Expression e = parser.parse("(((2+(2*x))/2)-10)");
         Expression de = e.derivative("x");
         final String standardOutput = de.toString();
-        assertTrue(standardOutput.contains("(0+((0*x)+(2*1)))"));
+        assertTrue(standardOutput.contains("(((((0+((0*x)+(2*1)))*2)-((2+(2*x))*0))/(2*2))-0)"));
     }
 
     @Test
