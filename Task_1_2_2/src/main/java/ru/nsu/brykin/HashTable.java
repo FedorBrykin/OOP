acd7ad559b64a9c4e5e15e3d3c6d15d12b1e5a85
RMS14579 <f.brykin@g.nsu.ru>
2024-10-11 12:55:59 +0700

    3rdtry

--- Task_1_2_2/src/main/java/ru/nsu/brykin/HashTable.java
+++ Task_1_2_2/src/main/java/ru/nsu/brykin/HashTable.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.AbstractMap;
 
+/**
+ * Hash-Table.
+ */
 public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
     private static final int CAPACITY = 4;
     private static final float RESIZE_F = 0.8f;
@@ -15,6 +18,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
     private int threshold;
     private int modCount;
 
+    /**
+     * Hash-Table.
+     */
     public HashTable() {
         this.table = new Entry[CAPACITY];
         this.threshold = (int) (CAPACITY * RESIZE_F);
@@ -22,6 +28,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         this.modCount = 0;
     }
 
+    /**
+     * entry.
+     */
     private static class Entry<K, V> {
         K key;
         V value;
@@ -32,6 +41,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         }
     }
 
+    /**
+     * добавление.
+     */
     public void put(K key, V value) {
         int index = getIndex(key);
         Entry<K, V> entry = table[index];
@@ -45,6 +57,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         addEntry(key, value, index);
     }
 
+    /**
+     * удаление.
+     */
     public void remove(K key) {
         int index = getIndex(key);
         Entry<K, V> previous = null;
@@ -65,6 +80,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         }
     }
 
+    /**
+     * поиск значения по ключу.
+     */
     public V get(K key) {
         int index = getIndex(key);
         Entry<K, V> entry = table[index];
@@ -77,20 +95,32 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         return null;
     }
 
+    /**
+     * обновление значения по ключу.
+     */
     public void update(K key, V value) {
         put(key, value);
     }
 
+    /**
+     * проверка наличия значения по ключу.
+     */
     public boolean containsKey(K key) {
         return get(key) != null;
     }
 
+    /**
+     * iterator.
+     */
     public Iterator<Map.Entry<K, V>> iterator() {
         return new Iterator<>() {
             private int currentIndex;
             private Entry<K, V> currentEntry;
             private final int expectedModCount = modCount;
 
+            /**
+             * наличие следующего.
+             */
             @Override
             public boolean hasNext() {
                 while (currentEntry == null && currentIndex < table.length) {
@@ -99,6 +129,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
                 return currentEntry != null;
             }
 
+            /**
+             * next.
+             */
             @Override
             public Map.Entry<K, V> next() {
                 if (expectedModCount != modCount) {
@@ -114,6 +147,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         };
     }
 
+    /**
+     * +запись.
+     */
     private void addEntry(K key, V value, int index) {
         if (size >= threshold) {
             resize();
@@ -126,10 +162,16 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         modCount++;
     }
 
+    /**
+     * индекс.
+     */
     private int getIndex(K key) {
         return key.hashCode() % table.length;
     }
 
+    /**
+     * resize.
+     */
     private void resize() {
         Entry<K, V>[] oldTable = table;
         table = new Entry[oldTable.length * 2];
@@ -143,6 +185,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         }
     }
 
+    /**
+     * toString.
+     */
     public String toString() {
         StringBuilder str = new StringBuilder("{");
         Iterator<Map.Entry<K, V>> iter = iterator();
@@ -157,6 +202,9 @@ public class HashTable<K, V> implements Iterable<Map.Entry<K,V>> {
         return str.toString();
     }
 
+    /**
+     * пример теста.
+     */
     public static void main(String[] args) {
         HashTable<String, Number> hashTable = new HashTable<>();
         hashTable.put("one", 1);
