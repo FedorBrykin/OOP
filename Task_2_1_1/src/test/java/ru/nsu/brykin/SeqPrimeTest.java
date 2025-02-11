29b1cddb3fad67cf083397bbc9201c7d26c4233c
Fedor <f.brykin@g.nsu.ru>
2025-02-11 09:40:35 +0700

    finally??

--- Task_2_1_1/src/test/java/ru/nsu/brykin/SeqPrimeTest.java
+++ Task_2_1_1/src/test/java/ru/nsu/brykin/SeqPrimeTest.java
@@ -1,13 +1,12 @@
 package ru.nsu.brykin;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  * тесты для послед.
