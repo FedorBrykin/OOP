64883b093d2fa73f1ae38a1c60d514febd7c1274
Fedor <f.brykin@g.nsu.ru>
2025-02-17 16:39:51 +0700

    2ndndtry

--- Task_2_2_1/src/test/java/ru/nsu/brykin/CourierTest.java
+++ Task_2_2_1/src/test/java/ru/nsu/brykin/CourierTest.java
@@ -7,7 +7,7 @@ class CourierTest {
     @Test
     void testCourier() throws InterruptedException {
         Storage storage = new Storage(10);
-        Courier courier = new Courier(1, 1, storage);
+        Courier courier = new Courier(1, 1, storage, null);
         Order order = new Order(1);
         storage.addOrder(order);
         courier.start();
