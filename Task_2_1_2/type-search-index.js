409167d7d0c713c968efc2be86b2ec647e2bdb44
FedorBrykin <FedorBrykin@users.noreply.github.com>
2025-04-02 11:20:55 +0000

    Deploying to gh-pages from @ FedorBrykin/OOP@dd341eb529f7ae09fd5f88afc255a442af489d6a 🚀

---
+++ Task_2_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.brykin","l":"PrimeCheckerMaster"},{"p":"ru.nsu.brykin","l":"PrimeCheckerWorker"}];updateSearchResults();
